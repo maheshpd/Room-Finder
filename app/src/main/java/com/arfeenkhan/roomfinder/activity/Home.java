362e713421959ac93e4b1da75406ff996d2a5b1b
Mahesh Prasad <mahesh223prasad@gmail.com>
2019-07-28 14:32:34 +0530

    set on bottom navigation item click listener

--- app/src/main/java/com/arfeenkhan/roomfinder/activity/Home.java
+++ app/src/main/java/com/arfeenkhan/roomfinder/activity/Home.java
@@ -1,16 +1,24 @@
 package com.arfeenkhan.roomfinder.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.arfeenkhan.roomfinder.R;
+import com.arfeenkhan.roomfinder.fragment.Discover;
+import com.arfeenkhan.roomfinder.fragment.Notification;
+import com.arfeenkhan.roomfinder.fragment.Profile;
+import com.arfeenkhan.roomfinder.fragment.Shortlist;
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Home extends AppCompatActivity {
 
     //Widget
-    BottomNavigationItemView bottomNavigationItemView;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +27,41 @@ public class Home extends AppCompatActivity {
 
         //inializer
         initWidget();
+        bottomNavigationView.setSelectedItemId(R.id.dsicover_menu);
+
     }
+
 
     private void initWidget() {
         //init widget
-        bottomNavigationItemView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
-//        bottomNavigationItemView.setOn
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            Fragment fragment = null;
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                if (menuItem.getItemId() == R.id.dsicover_menu)
+                    fragment = new Discover();
+                else if (menuItem.getItemId() == R.id.shortlist_menu)
+                    fragment = new Shortlist();
+                else if (menuItem.getItemId() == R.id.notification_menu)
+                    fragment = new Notification();
+                else if (menuItem.getItemId() == R.id.profile_menu)
+                    fragment = new Profile();
+
+                return loadFragment(fragment);
+            }
+        });
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.home_fragment_container, fragment).commit();
+            return true;
+
+        }
+        return false;
     }
 }
